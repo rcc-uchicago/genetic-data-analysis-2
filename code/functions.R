1e451eb9d0787ee862551f2c09fc0eba1ae2075d
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-03-05 13:19:32 -0600

    Added steps to verify phenotype residuals.

--- code/functions.R
+++ code/functions.R
@@ -1,3 +1,25 @@
+# Compare the empirical quantiles of x against the expected values
+# under the normal distribution.
+check.normal.quantiles <- function (x) {
+
+  # Discard the missing values.
+  x <- x[!is.na(x)]
+    
+  # Transform the observations so that they have zero mean and unit
+  # variance.
+  x <- (x - mean(x))/sqrt(var(x))
+  
+  # Create a data frame giving the observed and expected proportion of
+  # samples within 1, 2 and 3 standard deviations of the mean.
+  return(data.frame(exp = c(pnorm(1) - pnorm(-1),
+                            pnorm(2) - pnorm(-2),
+                            pnorm(3) - pnorm(-3)),
+                    obs = c(mean(-1 < x & x < 1),
+                            mean(-2 < x & x < 2),
+                            mean(-3 < x & x < 3)),
+                    row.names = c("sd1","sd2","sd3")))
+}
+
 # Function for reading the CFW phenotype data ("CFW_measures.txt") or
 # covariate data ("CFW_covariates.txt") from a tab-delimited file.
 read.pheno <- function (file)
@@ -30,3 +52,21 @@ draw.boxplot <- function (x, y)
   ggplot(data.frame(x = x,y = y),aes(x = x,y = y)) +
   geom_boxplot(width = 0.5,na.rm = TRUE) +
   theme(axis.line = element_blank())
+
+# Draw a quantile-quantile plot comparing the empirical quantiles
+# against quantiles expected under the standard normal distribution.
+draw.qqplot <- function (x) {
+
+  # Discard the missing values.
+  x <- x[!is.na(x)]
+    
+  # Transform the observations so that they have zero mean and unit
+  # variance.
+  x <- (x - mean(x))/sqrt(var(x))
+
+  return(ggplot(data.frame(x = x),aes(sample = x)) +
+         geom_abline(intercept = 0,slope = 1,color = "magenta") +
+         geom_qq(color = "darkblue") +
+         labs(x = "normal quantiles",y = "empirical quantiles") +
+         theme(axis.line = element_blank()))
+}
