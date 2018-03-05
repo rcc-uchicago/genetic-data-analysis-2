040ae98e7e2c16792431f0e17e372ea9feab3f60
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-03-05 11:16:14 -0600

    Added scatterplot to visualize tibia length vs. body weight.

--- code/functions.R
+++ code/functions.R
@@ -7,7 +7,19 @@ read.pheno <- function (file)
 # Visualize the empirical distribution of x using a histogram. Adjust
 # inputs "bins" and "color" as needed to improve the plot.
 plot.dist <- function (x, bins = 24, color = "darkblue")
-  ggplot(data.frame(x),aes(x)) +
+  ggplot(data.frame(x),aes(x),environment = environment()) +
     geom_histogram(bins = bins,fill = color,na.rm = TRUE) +
     theme(axis.line = element_blank())
       
+
+# Draw a scatterplot for visualizing the relationship of x and y. The
+# best linear fit between x and y is showns as a line in the scatterplot.
+draw.scatterplot <- function (x, y, size = 2) {
+  dat <- data.frame(x = x,y = y)
+  fit <- lm(y ~ x,dat)
+  return(ggplot(dat,aes(x = x,y = y),environment = environment()) +
+         geom_point(color = "darkblue",size = size,shape = 20,na.rm = TRUE) +
+         geom_abline(intercept = coef(fit)["(Intercept)"],
+                     slope = coef(fit)["x"],color = "magenta",size = 1) +
+         theme(axis.line = element_blank()))
+}
