6c846b87c65201d82078d71ce34b94a709523e85
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-03-05 11:55:02 -0600

    Added slides to visualize relationship between sex and tibia length.

--- code/functions.R
+++ code/functions.R
@@ -11,9 +11,9 @@ plot.dist <- function (x, bins = 24, color = "darkblue")
     geom_histogram(bins = bins,fill = color,na.rm = TRUE) +
     theme(axis.line = element_blank())
       
-
-# Draw a scatterplot for visualizing the relationship of x and y. The
-# best linear fit between x and y is showns as a line in the scatterplot.
+# Draw a scatterplot to visualize the relationship between two
+# continuous variables, x and y. The best linear fit between x and y is
+# shown as a line in the scatterplot.
 draw.scatterplot <- function (x, y, size = 2) {
   dat <- data.frame(x = x,y = y)
   fit <- lm(y ~ x,dat)
@@ -23,3 +23,12 @@ draw.scatterplot <- function (x, y, size = 2) {
                      slope = coef(fit)["x"],color = "magenta",size = 1) +
          theme(axis.line = element_blank()))
 }
+
+# Draw a box plot to visualize the relationship between a discrete
+# variable x and a continuous variable y.
+draw.boxplot <- function (x, y)
+  ggplot(data.frame(x = x,y = y),aes(x = x,y = y)) +
+  geom_boxplot(width = 0.5,na.rm = TRUE) +
+  theme(axis.line = element_blank())
+
+  
